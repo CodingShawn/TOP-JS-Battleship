599cb6c97d858718987b18a333ea3e424ea61ed5
Shawn <codingshawn@gmail.com>
2020-12-05 20:29:28 +0800

    Created createPlayer tests

---
+++ src/tests/createPlayer.test.js
@@ -0,0 +1,13 @@
+import gameboard from '../scripts/gameboard'
+import createPlayer from '../scripts/createPlayer'
+
+describe("Testing player functions", () =>{
+  let testBoard = gameboard();
+  let player = createPlayer(testBoard, false);
+  testBoard.placeShip(0, 0, 3, true);
+
+  test("Player is able to attack", () => {
+    player.playerMakeMove(0, 0);
+    expect(testBoard.checkLocation(0, 0)).toBe(true);
+  })
+})
