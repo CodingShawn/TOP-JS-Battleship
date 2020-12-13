4e8b90f2d40a89a52ee0d70d67ad8446bb901530
Shawn <codingshawn@gmail.com>
2020-12-13 20:25:36 +0800

    Added New Game button to start new game after game end

--- src/views/mainView.js
+++ src/views/mainView.js
@@ -1,3 +1,5 @@
+import pubsub from "../scripts/pubsub";
+
 function mainView() {
   let container = document.createElement("div");
   let body = document.getElementsByTagName("body")[0];
@@ -8,6 +10,29 @@ function mainView() {
   body.appendChild(pageTitle);
 
   body.appendChild(container);
+
+  let buttonContainer = createNewGameButton();
+  body.appendChild(buttonContainer);
+
+  pubsub.subscribe("GameEnd", function showNewGameButton() {
+    buttonContainer.classList.remove("hidden");
+  });
 }
 
 export default mainView;
+
+function createNewGameButton() {
+  let buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("button-container");
+  buttonContainer.classList.add("hidden");
+
+  let newGameButton = document.createElement("button");
+  newGameButton.textContent = "New Game";
+  newGameButton.classList.add("new-game-button");
+  newGameButton.addEventListener("click", function startNewGame() {
+    pubsub.publish("StartNewGame");
+  });
+
+  buttonContainer.appendChild(newGameButton);
+  return buttonContainer;
+}
